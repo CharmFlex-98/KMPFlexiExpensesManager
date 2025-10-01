0c3ef4605a48690383481d89ef97a28ecfd45253
LUK JIA MING <jiaming@LUKs-Air.lan>
2025-10-02 00:55:09 +0800

    config gix

--- composeApp/src/androidMain/kotlin/com/charmflex/cp/flexiexpensesmanager/core/app/AndroidDeviceInfoProvider.kt
+++ composeApp/src/androidMain/kotlin/com/charmflex/cp/flexiexpensesmanager/core/app/AndroidDeviceInfoProvider.kt
@@ -1,13 +1,19 @@
 package com.charmflex.cp.flexiexpensesmanager.core.app
 
 import android.content.res.Resources
-import java.util.Locale
 
 
 internal class AndroidDeviceInfoProvider : DeviceInfoProvider {
     override fun locale(): String {
-        val locale: Locale =
-            Resources.getSystem().configuration.locales[0]
-        return locale.toLanguageTag()
+        val locale = Resources.getSystem().configuration.locales[0]
+        val language = locale.language // "zh"
+        val country = locale.country // "CN"
+        val localeString = if (country.isNotEmpty()) {
+            language + "_" + country // "zh_CN"
+        } else {
+            language // "zh"
+        }
+
+        return localeString
     }
 }
