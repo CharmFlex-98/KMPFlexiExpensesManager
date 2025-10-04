02bfbb6a78478d6f1db3c0ac88c929a2dfdb8e61
LUK JIA MING <jiaming@LUKs-MacBook-Air.local>
2025-10-04 20:16:31 +0800

    update version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -161,8 +161,8 @@ android {
         applicationId = "com.charmflex.cp.flexiexpensesmanager"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 12
-        versionName = "1.0.12"
+        versionCode = 13
+        versionName = "1.0.13"
     }
 
 
